1a6a1322b076459c3b9651a61247103aa276dbb9
Akke <axel@latvala.com>
2016-01-11 21:56:15 +0200

    Initial commit

---
+++ mod_dagsen_weekly.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Dagsen weekly list module
+ * 
+ * @package    Joomla
+ * @subpackage Modules
+ * @license    MIT, see LICENSE.php
+ * @link       NULL
+ */
+ 
+// No direct access
+defined('_JEXEC') or die;
+// Include the syndicate functions only once
+require_once dirname(__FILE__) . '/helper.php';
+$taffaAPI = new taffaAPI('sv');
+$menuHTML = $taffaAPI->getNextMenu();
